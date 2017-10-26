319ef1a787a0521d6856625e650de29b48b53f95
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-26 18:50:49 +0300

    wrong env check

--- miniglm/__init__.py
+++ miniglm/__init__.py
@@ -13,7 +13,7 @@ from .glm import (
     mat4_perspective, mat4_ortho, mat4_look_at,
 )
 
-if os.environ.get('SPHINX') == 'yes':
+if os.environ.get('READTHEDOCS') == 'True':
     from .mock import (
         Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
         mat4_perspective, mat4_ortho, mat4_look_at,

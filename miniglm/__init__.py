715aadeaf969ea5536b73f9a48fc372f1a382724
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-26 18:54:03 +0300

    mock only

--- miniglm/__init__.py
+++ miniglm/__init__.py
@@ -8,13 +8,14 @@
 
 import os
 
-from .glm import (
-    Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
-    mat4_perspective, mat4_ortho, mat4_look_at,
-)
-
 if os.environ.get('READTHEDOCS') == 'True':
     from .mock import (
+        Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
+        mat4_perspective, mat4_ortho, mat4_look_at,
+    )
+
+else:
+    from .glm import (
         Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
         mat4_perspective, mat4_ortho, mat4_look_at,
     )

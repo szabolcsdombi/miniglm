c05724fa509422694468b9246f98a99c2ff10199
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-11-18 14:00:52 +0200

    tests

---
+++ tests/common.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MyTestTest(unittest.TestCase):
+    def assertAlmostEqual1(self, first, second, places=None, msg=None, delta=None):
+        if places is None:
+            places = 4
+
+        self.assertAlmostEqual(first, second, places, msg, delta)
+
+    def assertAlmostEqual2(self, first, second, places=None, msg=None, delta=None):
+        self.assertEqual(len(first), len(second))
+        for a, b in zip(first, second):
+            self.assertAlmostEqual1(a, b, places, msg, delta)
--- tests/test_vec2.py
+++ tests/test_vec2.py
@@ -2,47 +2,31 @@ import unittest
 import math
 
 from miniglm import Vec2
+from common import MyTestTest
 
 
-class TestCase(unittest.TestCase):
-    def test_example(self):
-        a = Vec2([3.0, 4.0])
+class TestCase(MyTestTest):
+    def test_1(self):
+        a = Vec2((1.0, 2.0))
+        b = Vec2((4.0, -4.0))
+        self.assertAlmostEqual2(a + b, (5.0, -2.0))
+        self.assertAlmostEqual2(a - b, (-3.0, 6.0))
+        self.assertAlmostEqual2(a * b, (4.0, -8.0))
+        self.assertAlmostEqual2(a / b, (0.25, -0.5))
 
-        ax, ay = a
-        self.assertAlmostEqual(ax, 3.0)
-        self.assertAlmostEqual(ay, 4.0)
+    def test_2(self):
+        a = Vec2((1.0, 2.0))
+        self.assertAlmostEqual2(a * 2, (2.0, 4.0))
+        self.assertAlmostEqual2(a / 2, (0.5, 1.0))
+        self.assertAlmostEqual2(+a, (1.0, 2.0))
+        self.assertAlmostEqual2(-a, (-1.0, -2.0))
 
-        self.assertAlmostEqual(a.length, 5.0)
-
-        b = Vec2([10.0, 10.0])
-
-        bnx, bny = b.normal
-        self.assertAlmostEqual(bnx, math.sqrt(2) / 2)
-        self.assertAlmostEqual(bny, math.sqrt(2) / 2)
-
-        self.assertAlmostEqual(a.dot(b), 70.0)
-
-        cx, cy = a + b * 2.0
-        self.assertAlmostEqual(cx, 23.0)
-        self.assertAlmostEqual(cy, 24.0)
-
-        light = Vec2([1.0, -1.0])
-
-        lx1, ly1 = light.reflect(Vec2([0.0, 1.0]))
-        self.assertAlmostEqual(lx1, 1.0)
-        self.assertAlmostEqual(ly1, 1.0)
-
-        lx2, ly2 = light.refract(Vec2([0.0, 1.0]), 0.0)
-        self.assertAlmostEqual(lx2, 0.0)
-        self.assertAlmostEqual(ly2, -1.0)
-
-        lx3, ly3 = light.refract(Vec2([0.0, 1.0]), 0.5)
-        self.assertAlmostEqual(lx3, 0.5)
-        self.assertAlmostEqual(ly3, -1.0)
-
-        lx4, ly4 = light.refract(Vec2([0.0, 1.0]), 1.0)
-        self.assertAlmostEqual(lx4, 1.0)
-        self.assertAlmostEqual(ly4, -1.0)
+    def test_3(self):
+        a = Vec2((1.0, 2.0))
+        self.assertAlmostEqual2(a, a.normal * a.length)
+        self.assertAlmostEqual2(a.normal, (0.4472135901451111, 0.8944271802902222))
+        self.assertAlmostEqual1(a.length, 2.2360680103302)
+        self.assertAlmostEqual1(a.normal.length, 1.0)
 
 
 if __name__ == '__main__':

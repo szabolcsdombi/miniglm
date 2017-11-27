9aaa5722841fd7a3b1ea4a637342ed03180b8130
Albert <frike98@yahoo.com>
2017-11-27 20:09:37 +0200

    Added test for Mat4 also swapped .col() and .row() because not column wise

--- src/matrix4.cpp
+++ src/matrix4.cpp
@@ -366,14 +366,14 @@ PyBufferProcs GLMMat4_tp_as_buffer = {
 PyObject * GLMMat4_tp_meth_row(GLMMat4 * self, PyObject * args) {
 	int idx = PyLong_AsLong(PyTuple_GetItem(args, 0));
 	GLMVec4 * res = (GLMVec4 *)GLMVec4_tp_new(&GLMVec4_Type, 0, 0);
-	res->val = glm::vec4(self->val[idx][0], self->val[idx][1], self->val[idx][2], self->val[idx][3]);
+	res->val = glm::vec4(self->val[0][idx], self->val[1][idx], self->val[2][idx], self->val[3][idx]);
 	return (PyObject *)res;
 }
 
 PyObject * GLMMat4_tp_meth_col(GLMMat4 * self, PyObject * args) {
 	int idx = PyLong_AsLong(PyTuple_GetItem(args, 0));
 	GLMVec4 * res = (GLMVec4 *)GLMVec4_tp_new(&GLMVec4_Type, 0, 0);
-	res->val = glm::vec4(self->val[0][idx], self->val[1][idx], self->val[2][idx], self->val[3][idx]);
+	res->val = glm::vec4(self->val[idx][0], self->val[idx][1], self->val[idx][2], self->val[idx][3]);
 	return (PyObject *)res;
 }
 

f4f3b46105ae39c963c0d5fc6e45e886de7a8f1a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-12-04 16:19:51 +0200

    test mat4 perspective

--- tests/test_mat4_functions.py
+++ tests/test_mat4_functions.py
@@ -6,13 +6,20 @@ from common import MyTestCase
 
 
 class TestCase(MyTestCase):
-    def test_1(self):
-        self.skipTest('TODO: glm::perspective vs pyrr.perspective confict')
-        a = mat4_perspective(80, 1.77, 1.0, 1000.0)
-        self.assertAlmostEqual2(a, (0.67330711446, 0.0 ,0.0, 0.0, 0.0, 1.19175359259, 0.0, 0.0, 0.0, 0.0, -1.002002002, -1.0, 0.0, 0.0, -2.002002002, 0.0))
+    def test_mat4_perspective(self):
+        projection = mat4_perspective(90, 1.0, 1.0, 10.0)
+        self.assertAlmostEqual2(projection, (0.617369, 0.0, 0.0, 0.0, 0.0, 0.617369, 0.0, 0.0, 0.0, 0.0, -1.222222, -1.0, 0.0, 0.0, -2.222222, 0.0))
 
-    def test_2(self):
+        projection = mat4_perspective(90, 2.0, 1.0, 10.0)
+        self.assertAlmostEqual2(projection, (0.308684, 0.0, 0.0, 0.0, 0.0, 0.617369, 0.0, 0.0, 0.0, 0.0, -1.222222, -1.0, 0.0, 0.0, -2.222222, 0.0))
 
+        projection = mat4_perspective(60, 1.5, 1.0, 10.0)
+        self.assertAlmostEqual2(projection, (-0.104079, 0.0, 0.0, 0.0, 0.0, -0.156119, 0.0, 0.0, 0.0, 0.0, -1.222222, -1.0, 0.0, 0.0, -2.222222, 0.0))
+
+        projection = mat4_perspective(60, 1.5, 0.1, 100.0)
+        self.assertAlmostEqual2(projection, (-0.104079, 0.0, 0.0, 0.0, 0.0, -0.156119, 0.0, 0.0, 0.0, 0.0, -1.002002, -1.0, 0.0, 0.0, -0.200200, 0.0))
+
+    def test_mat4_look_at(self):
         a = mat4_look_at(
             (cos(0.5) * 5.0, sin(0.5) * 5.0, 2.0),
             (0.0, 0.0, 0.5),
@@ -20,8 +27,7 @@ class TestCase(MyTestCase):
         )
         self.assertAlmostEqual2(a, (-0.4794255495071411, -0.25217148661613464, 0.8405716419219971, 0.0, 0.8775825500488281, -0.1377619206905365, 0.45920637249946594, 0.0, 0.0, 0.9578262567520142, 0.2873478829860687, 0.0, 2.384185791015625e-07, -0.4789130687713623, -5.363826751708984, 1.0))
 
-
-    def test_3(self):
+    def test_mat4_ortho(self):
         a = mat4_ortho(-1.0, 1.0, 1.0, -1.0, 1.0, 1000.0)
         self.assertAlmostEqual2(a, (1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, -0.002002002002, 0.0, -0.0, 0.0, -1.002002002, 1.0))
 

41a58e621df107942d78f5fb5f0ebfb3355b1d3a
AlphaZoneR <frike98@yahoo.com>
2017-12-08 19:07:56 +0200

    Updated Mat2Arrays to use FloatArray

--- src/matrix2_array.cpp
+++ src/matrix2_array.cpp
@@ -519,12 +519,14 @@ PyObject * GLMMat2Array_tp_get_trans(GLMMat2Array * self, void * closure){
 
 PyObject * GLMMat2Array_tp_get_det(GLMMat2Array * self, void * closure){
     int size = self->size;
-    PyObject * res = PyTuple_New(size);
+    GLMFloatArray * res = (GLMFloatArray *)GLMFloatArray_tp_new(&GLMFloatArray_Type, 0, 0);
+    res->size = size;
+    res->val = new float[size + 1];
 
     for(int i = 0; i < size; ++i){
-        PyTuple_SET_ITEM(res, i, PyFloat_FromDouble(glm::determinant(self->val[i])));
+        res->val[i] = glm::determinant(self->val[i]);
     }
-    return res; 
+    return (PyObject *)res; 
 }
 
 PyObject * GLMMat2Array_tp_get_inv(GLMMat2Array * self, void * closure){

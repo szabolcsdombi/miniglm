5f67e01698d89e3e071304df07f55147fa93367a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-01-30 08:06:32 +0200

    subtract multiply normalize

--- mymodule.cpp
+++ mymodule.cpp
@@ -11,6 +11,34 @@ PyObject * mymodule_meth_add(PyObject * self, PyObject * args) {
     return Py_BuildValue("ddd", c.x, c.y, c.z);
 }
 
+PyObject * mymodule_meth_sub(PyObject * self, PyObject * args) {
+    glm::dvec3 a, b;
+    if (!PyArg_ParseTuple(args, "(ddd)(ddd)", &a.x, &a.y, &a.z, &b.x, &b.y, &b.z)) {
+        return NULL;
+    }
+    glm::dvec3 c = a - b;
+    return Py_BuildValue("ddd", c.x, c.y, c.z);
+}
+
+PyObject * mymodule_meth_mul(PyObject * self, PyObject * args) {
+    glm::dvec3 a;
+    double f;
+    if (!PyArg_ParseTuple(args, "(ddd)d", &a.x, &a.y, &a.z, &f)) {
+        return NULL;
+    }
+    glm::dvec3 b = a * f;
+    return Py_BuildValue("ddd", b.x, b.y, b.z);
+}
+
+PyObject * mymodule_meth_normalize(PyObject * self, PyObject * args) {
+    glm::dvec3 v;
+    if (!PyArg_ParseTuple(args, "(ddd)", &v.x, &v.y, &v.z)) {
+        return NULL;
+    }
+    glm::dvec3 n = glm::normalize(v);
+    return Py_BuildValue("ddd", n.x, n.y, n.z);
+}
+
 PyObject * mymodule_meth_quat2mat(PyObject * self, PyObject * args) {
     glm::dquat q;
     if (!PyArg_ParseTuple(args, "(dddd)", &q.x, &q.y, &q.z, &q.w)) {
